b2ba8237fcceb208ac24cd6e506102a17534af22
Paranat Klinhom <tl.topspin.48@gmail.com>
2024-06-23 20:13:56 +0700

    initial commit

--- src/modules/Authentication/component/AuthenticationForm.tsx
+++ src/modules/Authentication/component/AuthenticationForm.tsx
@@ -18,41 +18,39 @@ interface IAuthenticationFormProps {
 function AuthenticationForm(props: IAuthenticationFormProps) {
   const { onFinishedForm } = props
   return (
-    <div>
-      <Form
-        className="login-form lg:w-[10rem] sm:w-1/4 w-full"
-        initialValues={{ remember: true }}
-        onFinish={(ver: Form) => {
-          onFinishedForm(ver)
-        }}
+    <Form
+      className="login-form lg:w-[10rem] sm:w-1/4 w-full"
+      initialValues={{ remember: true }}
+      onFinish={(ver: any) => {
+        onFinishedForm(ver)
+      }}
+    >
+      <Form.Item
+        name="email"
+        rules={[{ required: true, message: 'กรุณากรอกชื่อผู้ใช้งาน!' }]}
       >
-        <Form.Item
-          name="email"
-          rules={[{ required: true, message: 'กรุณากรอกชื่อผู้ใช้งาน!' }]}
-        >
-          <Input
-            prefix={<UserOutlined className="site-form-item-icon" />}
-            placeholder="Username"
-          />
-        </Form.Item>
-        <Form.Item
-          name="password"
-          rules={[{ required: true, message: 'กรุณากรอกรหัสผ่าน!' }]}
-        >
-          <Input
-            prefix={<LockOutlined className="site-form-item-icon" />}
-            type="password"
-            placeholder="Password"
-          />
-        </Form.Item>
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Username"
+        />
+      </Form.Item>
+      <Form.Item
+        name="password"
+        rules={[{ required: true, message: 'กรุณากรอกรหัสผ่าน!' }]}
+      >
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+        />
+      </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" className="login-form-button w-full">
-            เข้าสู่ระบบ
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" className="login-form-button w-full">
+          เข้าสู่ระบบ
+        </Button>
+      </Form.Item>
+    </Form>
   )
 }
 
